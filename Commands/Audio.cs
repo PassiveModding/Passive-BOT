1d4e542845b0c26c2c6113d1f3f28dd0a68ff7de
PassiveModding <passivemodding@gmail.com>
2017-06-05 23:47:40 +1000

    More Audio Fixes and Changes
    Ordered all music commands to function through a queue
    Commands:
    join
    leave
    songs
    queue
    q play
    q all
    q pl
    q add
    q skip
    q del
    q clear
    
    Enjoy!

--- Commands/Audio.cs
+++ Commands/Audio.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,74 +25,55 @@ namespace PassiveBOT.Commands
         }
 
         [Command("join", RunMode = RunMode.Async)]
-        [Alias("j")]
-        [Summary("j")]
+        [Summary("join")]
         [Remarks("Joins your Voice Channel")]
         public async Task JoinCmd()
         {
-            await ReplyAsync($"{Context.Client.CurrentUser.Username} is joining **{Context.Channel}**");
+            await ReplyAsync("Joining Audio Channel");
             await _service.JoinAudio(Context.Guild, (Context.User as IVoiceState).VoiceChannel);
         }
 
         [Command("leave", RunMode = RunMode.Async)]
-        [Alias("l")]
-        [Summary("l")]
+        [Summary("loin")]
         [Remarks("Leaves your Voice Channel")]
         public async Task LeaveCmd()
         {
             await _service.LeaveAudio(Context.Guild);
-            await ReplyAsync($"{Context.Client.CurrentUser.Username} is Leaving **{Context.Channel}**");
+            await ReplyAsync("Leaving Audio Channel");
         }
 
-        [Command("play", RunMode = RunMode.Async)]
-        [Alias("p")]
-        [Summary("p")]
-        [Remarks("Plays the requested Song")]
-        public async Task PlayCmd([Remainder] string linkOrSearchTerm)
+        [Command("songs", RunMode = RunMode.Async)]
+        [Summary("songs")]
+        [Remarks("Lists all songs downloaded in your server")]
+        public async Task SongList()
         {
-            await _service.LeaveAudio(Context.Guild);
-            await _service.JoinAudio(Context.Guild, (Context.User as IVoiceState).VoiceChannel);
-            await _service.SendAudioAsync(Context.Guild, Context.Channel, linkOrSearchTerm);
-            await _service.LeaveAudio(Context.Guild);
-            await ReplyAsync($"{Context.Client.CurrentUser.Username} is Leaving **{Context.Channel}**");
-        }
+            //gets the current guilds directory
+            if (Directory.Exists($"{AppContext.BaseDirectory}/music/{Context.Guild.Id}/"))
+            {
+                var d = new DirectoryInfo($"{AppContext.BaseDirectory}/music/{Context.Guild.Id}/");
+                var music = d.GetFiles("*.*");
+                var songlist = new List<string>();
+                var i = 0;
+                foreach (var sng in music)
+                {
+                    songlist.Add($"`{i}` - {Path.GetFileNameWithoutExtension(sng.Name)}");
+                    i++;
+                }
 
-        [Command("playlist", RunMode = RunMode.Async)]
-        [Alias("pl")]
-        [Summary("pl")]
-        [Remarks("Plays the given playlist")]
-        public async Task PlaylistCmd([Remainder] string playlistLink)
-        {
-            await _service.LeaveAudio(Context.Guild);
-            await _service.JoinAudio(Context.Guild, (Context.User as IVoiceState).VoiceChannel);
+                var list = string.Join("\n", songlist.ToArray());
 
-            var ytc = new YoutubeClient();
-
-            var playListInfo = await ytc.GetPlaylistInfoAsync(YoutubeClient.ParsePlaylistId(playlistLink));
-
-            var idArray = playListInfo.VideoIds.ToArray();
-
-            foreach (var id in idArray)
-                await _service.SendAudioAsync(Context.Guild, Context.Channel, id);
-            await _service.LeaveAudio(Context.Guild);
+                await ReplyAsync(list);
+            }
+            else
+            {
+                await ReplyAsync("There are currently no songs downloaded for this guild\n" +
+                                 $"you can download songs using the `{Load.Pre}play` command");
+            }
         }
 
         [Command("queue", RunMode = RunMode.Async)]
         [Alias("q")]
         [Summary("q")]
-        [Remarks("Adds a song to the queue")]
-        public async Task QueueSong([Remainder] string linkOrSearchTerm)
-        {
-            Queue.Add(linkOrSearchTerm);
-            _reply = Queue.Count == 1
-                ? "There is 1 song in the queue."
-                : $"There are **{Queue.Count}** songs in the queue.";
-            await ReplyAsync($"**{linkOrSearchTerm}** added.\n{_reply}");
-        }
-
-        [Command("list", RunMode = RunMode.Async)]
-        [Alias("li")]
-        [Summary("li")]
         [Remarks("Lists all songs in the queue")]
         public async Task QueueList()
         {
@@ -111,19 +94,9 @@ namespace PassiveBOT.Commands
             }
         }
 
-        [Command("clearque", RunMode = RunMode.Async)]
-        [Alias("clearq")]
-        [Summary("clearq")]
-        [Remarks("Empties the queue")]
-        public async Task ClearQue()
-        {
-            Queue.Clear();
-            await ReplyAsync("Queue has been cleared");
-        }
-
-        [Command("playqueue", RunMode = RunMode.Async)]
-        [Alias("playq")]
-        [Summary("playq")]
+        [Command("q play", RunMode = RunMode.Async)]
+        [Alias("queue play")]
+        [Summary("q play 'YT video'/'YT video name'")]
         [Remarks("Plays the queue")]
         public async Task PlayQueue()
         {
@@ -145,17 +118,101 @@ namespace PassiveBOT.Commands
             await ReplyAsync($"Sorry, the queue is empty, {Load.Pre}queue (or {Load.Pre}q) to add more!");
 
             await _service.LeaveAudio(Context.Guild);
-            await ReplyAsync($"{Context.Client.CurrentUser.Username} is Leaving **{Context.Channel}**");
+            await ReplyAsync("Leaving Audio Channel");
         }
 
-        [Command("skip", RunMode = RunMode.Async)]
-        [Summary("skip")]
-        [Remarks("Removes the given song from the queue")]
-        public async Task SkipSong([Remainder] int x = 0)
+        [Command("q all", RunMode = RunMode.Async)]
+        [Alias("queue all")]
+        [Summary("pa")]
+        [Remarks("Plays all downloaded songs")]
+        public async Task Pall()
         {
-            if (Queue.Count > 0 && x < Queue.Count && x > -1)
+            await _service.LeaveAudio(Context.Guild);
+            await _service.JoinAudio(Context.Guild, (Context.User as IVoiceState).VoiceChannel);
+
+            if (Directory.Exists($"{AppContext.BaseDirectory}/music/{Context.Guild.Id}/"))
+            {
+                var d = new DirectoryInfo($"{AppContext.BaseDirectory}/music/{Context.Guild.Id}/");
+                var music = d.GetFiles("*.*");
+                foreach (var sng in music)
+                {
+                    Queue.Add(Path.GetFileNameWithoutExtension(sng.Name));
+                }
+                await PlayQueue();
+            }
+            else
+            {
+                await ReplyAsync("There are no songs downloaded in this server yet");
+            }
+        }
+
+        [Command("q pl", RunMode = RunMode.Async)]
+        [Alias("q playlist", "queue playlist", "queue pl")]
+        [Summary("q pl 'playlist url'")]
+        [Remarks("Adds the given YT playlist to the Queue")]
+        public async Task PlaylistCmd([Remainder] string playlistLink)
+        {
+            await _service.LeaveAudio(Context.Guild);
+            await _service.JoinAudio(Context.Guild, (Context.User as IVoiceState).VoiceChannel);
+
+            var ytc = new YoutubeClient();
+
+            var playListInfo = await ytc.GetPlaylistInfoAsync(YoutubeClient.ParsePlaylistId(playlistLink));
+            var idArray = playListInfo.VideoIds.ToArray();
+            var twenty = idArray.Take(10).ToArray();
+            foreach (var song in twenty)
+            {
+                var videoInfo = await ytc.GetVideoInfoAsync(song);
+                var title = videoInfo.Title;
+                Queue.Add(title);
+            }
+            await PlayQueue();
+        }
+
+        [Command("q add", RunMode = RunMode.Async)]
+        [Alias("queue add")]
+        [Summary("q add 'yt video'/'yt video name'")]
+        [Remarks("Adds a song to the queue")]
+        public async Task QueueSong([Remainder] string linkOrSearchTerm)
+        {
+            Queue.Add(linkOrSearchTerm);
+            _reply = Queue.Count == 1
+                ? "There is 1 song in the queue."
+                : $"There are **{Queue.Count}** songs in the queue.";
+            await ReplyAsync($"**{linkOrSearchTerm}** added.\n{_reply}");
+        }
+
+        [Command("q skip", RunMode = RunMode.Async)]
+        [Alias("queue skip")]
+        [Summary("q skip")]
+        [Remarks("Skips the current song")]
+        public async Task SkipSong()
+        {
+            if (Queue.Count > 0)
+                Queue.RemoveAt(0);
+            await PlayQueue();
+        }
+
+        [Command("q del", RunMode = RunMode.Async)]
+        [Alias("queue del", "q delete", "queue delete")]
+        [Summary("q del 'x'")]
+        [Remarks("Removes the given song from the queue")]
+        public async Task Qdel(int x)
+        {
+            await ReplyAsync($"Removed **{Queue.ElementAt(x)}** from the queue");
+            if (Queue.Count > 0)
                 Queue.RemoveAt(x);
-            await ReplyAsync($"Removed the Song `#{x}` from the Queue");
+
+        }
+
+        [Command("q clear", RunMode = RunMode.Async)]
+        [Alias("queue clear")]
+        [Summary("q clear")]
+        [Remarks("Empties the queue")]
+        public async Task ClearQue()
+        {
+            Queue.Clear();
+            await ReplyAsync("Queue has been cleared");
         }
     }
 }
