d5cd216575670f7757390ae01444643582e9c908
Passive <passivemodding@gmail.com>
2018-04-04 07:25:19 +1000

    .

--- Configuration/Load.cs
+++ Configuration/Load.cs
@@ -12,10 +12,7 @@ namespace PassiveBOT.Configuration
         public static int Messages;
         public static int Commands;
         public static string DBLLink = "https://discordbots.org/bot/303710071387324416";
-
-        public static string Invite = $"https://discordapp.com/oauth2/authorize?client_id={Program.Client.CurrentUser.Id}&scope=bot&permissions=2146958591";
-
-
+        public static string Invite = $"https://discordapp.com/oauth2/authorize?client_id=430837105690673152&scope=bot&permissions=2146958591";
         public static string Server =  Config.Load().SupportServer;
     }
 }
