37fd32792bbf21055386bfa03df827330054655c
PassiveModding <passivemodding@gmail.com>
2018-05-06 22:43:31 +1000

    Better Calculator
    Added support for e and powers

--- Commands/Text/Chat.cs
+++ Commands/Text/Chat.cs
@@ -1,5 +1,8 @@
-﻿using System.Data;
+﻿using System;
+using System.Data;
+using System.Globalization;
 using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
@@ -23,11 +26,88 @@ namespace PassiveBOT.Commands.Text
 
         [Command("calculate")]
         [Summary("calculate <math expression>")]
-        [Remarks("calculates the given input")]
+        [Remarks("calculates the given input. use brackets where possible in complex calculations.")]
         public async Task Calculate([Remainder] string input)
         {
-            var output = new DataTable().Compute(input, null).ToString();
-            await ReplyAsync($"{input} = {output}");
+              
+            input = input.Replace(" ", "").ToLower();
+            var working = $"{input}\n";
+
+            //Replace special integers
+            if (input.Contains(")("))
+            {
+                input = input.Replace(")(", ")*(");
+                working += $"{input}\n";
+            }
+
+            foreach (var section in Regex.Matches(input, @"\([\S\s]+?\)"))
+            {
+                var sec = section.ToString().Replace("(", "").Replace(")", "");
+                //Replace special integers
+                if (sec.Contains("e"))
+                {
+                    sec = Regex.Replace(sec, @"([0-9])e", $"$1*{Math.E}");
+                    sec = Regex.Replace(sec, @"e([0-9])", $"{Math.E}*$1");
+                    sec = sec.Replace("e", $"{Math.E}");
+                    working += $"simplify: {section} => {sec}\n";
+                }
+
+                var sec1 = sec;
+                //Fix Powers
+                if (sec.Contains("^"))
+                {
+                    var matches = Regex.Matches(sec, @"[-+]?([0-9]*\.[0-9]+|[0-9]+)\^[-+]?([0-9]*\.[0-9]+|[0-9]+)");
+                    foreach (var match in matches)
+                    {
+                        var intlist = Regex.Matches(match.ToString(), @"[-+]?([0-9]*\.[0-9]+|[0-9]+)");
+                        var intbase = double.Parse(intlist[0].ToString());
+                        var intpow = double.Parse(intlist[1].ToString());
+                        var sum = Math.Pow(intbase, intpow);
+                        sec = sec.Replace(match.ToString(), sum.ToString(CultureInfo.InvariantCulture));
+                    }
+                    working = $"simplify: {sec1} => {sec}\n";
+                }
+
+                sec = new DataTable().Compute(sec, null).ToString();
+                working += $"{section} => {sec}\n";
+                input = input.Replace(section.ToString(), sec);
+                working += $"{input}\n";
+            }
+
+                //Replace special integers
+                if (input.Contains("e"))
+                {
+                    input = Regex.Replace(input, @"([0-9])e", $"$1*{Math.E}");
+                    input = Regex.Replace(input, @"e([0-9])", $"{Math.E}*$1");
+                    input = input.Replace("e", $"{Math.E}");
+                    working += $"{input}\n";
+                }
+
+
+
+                //Fix Powers
+                if (input.Contains("^"))
+                {
+                    var matches = Regex.Matches(input, @"[-+]?([0-9]*\.[0-9]+|[0-9]+)\^[-+]?([0-9]*\.[0-9]+|[0-9]+)");
+                    foreach (var match in matches)
+                    {
+                        var intlist = Regex.Matches(match.ToString(), @"[-+]?([0-9]*\.[0-9]+|[0-9]+)");
+                        var intbase = double.Parse(intlist[0].ToString());
+                        var intpow = double.Parse(intlist[1].ToString());
+                        var sum = Math.Pow(intbase, intpow);
+                        input = input.Replace(match.ToString(), sum.ToString(CultureInfo.InvariantCulture));
+                    }
+
+                    working += $"{input}\n";
+                }
+
+                var output = new DataTable().Compute(input, null).ToString();
+
+                await ReplyAsync($"__**Working**__\n" +
+                                 $@"{working.Replace("*", @"\*")}" +
+                                 $"\n" +
+                                 $"__**Final**__\n" +
+                                 $@"{Regex.Escape(input)} = {output}");
         }
 
         [Command("quote")]
