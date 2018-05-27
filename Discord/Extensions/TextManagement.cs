2878a089f1f87864c6a3be101c4a941c3a854b3a
PassiveModding <passivemodding@gmail.com>
2018-05-27 13:37:39 +1000

    Added Rank and Leaderboard Commands

---
+++ Discord/Extensions/TextManagement.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PassiveBOT.Discord.Extensions
+{
+    public class TextManagement
+    {
+        public static List<List<T>> splitList<T>(List<T> FullList, int GroupSize = 30)
+        {
+            var newlist = new List<List<T>>();
+            for (int i = 0; i < FullList.Count; i += GroupSize)
+            {
+                newlist.Add(FullList.Skip(i).Take(GroupSize).ToList());
+            }
+
+            return newlist;
+        }
+    }
+}
