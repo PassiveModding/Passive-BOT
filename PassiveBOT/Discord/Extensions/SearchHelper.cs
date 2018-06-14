1d727d33ec9963f7ea75647d60ffc66889af6f6c
PassiveModding <passivemodding@gmail.com>
2018-06-14 23:53:01 +1000

    Added media commands

---
+++ PassiveBOT/Discord/Extensions/SearchHelper.cs
@@ -0,0 +1,97 @@
+﻿namespace PassiveBOT.Discord.Extensions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// The search helper.
+    /// </summary>
+    public class SearchHelper
+    {
+        /// <summary>
+        /// The request http method.
+        /// </summary>
+        public enum RequestHttpMethod
+        {
+            Get,
+            Post
+        }
+        /// <summary>
+        /// The get response string async.
+        /// </summary>
+        /// <param name="url">
+        /// The url.
+        /// </param>
+        /// <param name="headers">
+        /// The headers.
+        /// </param>
+        /// <param name="method">
+        /// The method.
+        /// </param>
+        /// <returns>
+        /// The <see cref="Task"/>.
+        /// </returns>
+        public static async Task<string> GetResponseStringAsync(string url, IEnumerable<KeyValuePair<string, string>> headers = null, RequestHttpMethod method = RequestHttpMethod.Get)
+        {
+            using (var streamReader =
+                new StreamReader(await GetResponseStreamAsync(url, headers, method).ConfigureAwait(false)))
+            {
+                return await streamReader.ReadToEndAsync().ConfigureAwait(false);
+            }
+        }
+
+        /// <summary>
+        /// The get response stream async.
+        /// </summary>
+        /// <param name="url">
+        /// The url.
+        /// </param>
+        /// <param name="headers">
+        /// The headers.
+        /// </param>
+        /// <param name="method">
+        /// The method.
+        /// </param>
+        /// <returns>
+        /// The <see cref="Task"/>.
+        /// </returns>
+        /// <exception cref="Exception">
+        /// Throws if incorrect method input.
+        /// </exception>
+        private static async Task<Stream> GetResponseStreamAsync(string url,
+            IEnumerable<KeyValuePair<string, string>> headers = null, RequestHttpMethod method = RequestHttpMethod.Get)
+        {
+            var cl = new HttpClient();
+            cl.DefaultRequestHeaders.Clear();
+            switch (method)
+            {
+                case RequestHttpMethod.Get:
+                    if (headers == null)
+                    {
+                        return await cl.GetStreamAsync(url).ConfigureAwait(false);
+                    }
+
+                    foreach (var header in headers)
+                    {
+                        cl.DefaultRequestHeaders.TryAddWithoutValidation(header.Key, header.Value);
+                    }
+
+                    return await cl.GetStreamAsync(url).ConfigureAwait(false);
+                case RequestHttpMethod.Post:
+                    FormUrlEncodedContent formContent = null;
+                    if (headers != null)
+                    {
+                        formContent = new FormUrlEncodedContent(headers);
+                    }
+
+                    var message = await cl.PostAsync(url, formContent).ConfigureAwait(false);
+                    return await message.Content.ReadAsStreamAsync().ConfigureAwait(false);
+                default:
+                    throw new Exception("That type of request is unsupported.");
+            }
+        }
+    }
+}
---
+++ PassiveBOT/Models/RedditModels.cs
@@ -0,0 +1,49 @@
+﻿namespace PassiveBOT.Models
+{
+    using System;
+    using System.Collections.Generic;
+
+    using RedditSharp.Things;
+
+    /// <summary>
+    /// The reddit models.
+    /// </summary>
+    public class RedditModels
+    {
+        /// <summary>
+        /// Gets or sets sub reddits.
+        /// </summary>
+        public static List<SubReddit> SubReddits { get; set;  }= new List<SubReddit>();
+
+        /// <summary>
+        /// The subreddit module
+        /// </summary>
+        public class SubReddit
+        {
+            /// <summary>
+            /// Gets or sets the title.
+            /// </summary>
+            public string Title { get; set; }
+
+            /// <summary>
+            /// Gets or sets the posts.
+            /// </summary>
+            public List<Post> Posts { get; set; }
+
+            /// <summary>
+            /// Gets or sets the last update.
+            /// </summary>
+            public DateTime LastUpdate { get; set; }
+
+            /// <summary>
+            /// Gets or sets the hits.
+            /// </summary>
+            public int Hits { get; set; } = 0;
+
+            /// <summary>
+            /// Gets or sets a value indicating whether nsfw.
+            /// </summary>
+            public bool NSFW { get; set; } = false;
+        }
+    }
+}
